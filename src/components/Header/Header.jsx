8973e7d36d8e04f5455873b0e06637e6f8271a00
vatsal <rajvatsal2004@gmail.com>
2024-12-24 15:15:23 +0530

    feat: Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
+import "./styles.scss";
+
 export function Header() {
   return(
     <header className="header">
-      <div>
-        <div>Logo</div>
-        <div>Name</div>
-      </div>
+        <img className="header__icn" src="/Logo.svg" alt="logo" />
+        <h1 className="header__title">CV GENERATOR</h1>
+        <span className="header__desc">Create A Professional CV In Seconds</span>
     </header>
   );
 }
