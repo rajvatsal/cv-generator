3a62c6261b2c8b19ad4ae7d7a1229eb5f5275a76
vatsal <rajvatsal2004@gmail.com>
2024-12-26 18:25:23 +0530

    refactor(Main.jsx): move render logic to Main from Templates

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,36 @@
 import './Main.scss'
-import { InitRender } from '../Templates/Templates.jsx'
+import { templates } from '../Templates/Templates.jsx'
+import { useState, useCallback } from 'react'
+import { CommonControls } from '../Templates/CommonControls.jsx'
+
+function ControlsContainer({ setTemplate, children }) {
+  return (
+    <div className="main__controls">
+      {children}
+      <CommonControls setTemplate={setTemplate} />
+    </div>
+  )
+}
+
+function PreviewContainer({ children }) {
+  return <div className="main__previewer">{children}</div>
+}
 
 export function Main() {
+  const [template, setTemplate] = useState('1')
+  const updateTemplate = useCallback((val) => setTemplate(val))
+
+  const { Controls, Previewer } = templates.find(
+    (_, i) => i === Number.parseInt(template) - 1
+  )
   return (
     <main className="main">
-      <InitRender />
+      <ControlsContainer setTemplate={updateTemplate}>
+        <Controls />
+      </ControlsContainer>
+      <PreviewContainer>
+        <Previewer />
+      </PreviewContainer>
     </main>
   )
 }
--- src/components/Templates/Templates.jsx
+++ src/components/Templates/Templates.jsx
@@ -3,40 +3,5 @@ import './Templates.scss'
 import tmpl_1 from './TemplateOne/main.jsx'
 import tmpl_2 from './TemplateTwo/main.jsx'
 import tmpl_3 from './TemplateThree/main.jsx'
-import { useState, useCallback } from 'react'
-import { CommonControls } from './CommonControls.jsx'
 
-const templates = [tmpl_1, tmpl_2, tmpl_3]
-
-function ControlsContainer({ setTemplate, children }) {
-  return (
-    <div className="main__controls">
-      {children}
-      <CommonControls setTemplate={setTemplate} />
-    </div>
-  )
-}
-
-function PreviewContainer({ children }) {
-  return <div className="main__previewer">{children}</div>
-}
-
-export function InitRender() {
-  const [template, setTemplate] = useState('1')
-  const updateTemplate = useCallback((val) => setTemplate(val))
-
-  const { Controls, Previewer } = templates.find(
-    (_, i) => i === Number.parseInt(template) - 1
-  )
-
-  return (
-    <>
-      <ControlsContainer setTemplate={updateTemplate}>
-        <Controls />
-      </ControlsContainer>
-      <PreviewContainer>
-        <Previewer />
-      </PreviewContainer>
-    </>
-  )
-}
+export const templates = [tmpl_1, tmpl_2, tmpl_3]
