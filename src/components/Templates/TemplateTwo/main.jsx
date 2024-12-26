5b2cd76d64095d00412df793020d92b7ba0d71c7
vatsal <rajvatsal2004@gmail.com>
2024-12-26 18:26:52 +0530

    refactor(templates): delete unused components

--- src/components/Templates/TemplateTwo/main.jsx
+++ src/components/Templates/TemplateTwo/main.jsx
@@ -1,13 +1,4 @@
 import { Previewer } from './previewer.jsx'
 import { Controls } from './controls.jsx'
 
-function Template(props) {
-  return (
-    <>
-      <Controls {...props} />
-      <Previewer />
-    </>
-  )
-}
-
 export default { Previewer, Controls }
